7565e08d8fa1ae5553415a797e338e7bc550a5f5
unknown <roi.hakim@gmail.com>
2021-11-15 18:29:17 +0200

    test

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const provider = ethers.provider.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
 function addToWhitelist() {
+    $("#txStatus").text("Creating whitlist message and sig");
+
     let address = document.getElementById("addressInput").value;
 
     let signature = await signer.signMessage(address + "airdrop").then(alert(signature) )
