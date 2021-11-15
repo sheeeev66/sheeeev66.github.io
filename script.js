df834b39b462a0f20ee2f89469423ab490cf800c
unknown <roi.hakim@gmail.com>
2021-11-15 17:36:56 +0200

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+import { ethers } from "ethers.js";
+// Your code here...
+const provider = ethers.provider.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+function addToWhitelist() {
+    let address = document.getElementById("addressInput").value;
+
+    let signature = await signer.signMessage(address + "airdrop").then(alert(signature) )
+    
+}
